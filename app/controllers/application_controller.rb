40f6bf8e67dcbd0d1d155512f39fb0166d156669
Ken Iovino <kiovino@verticalscope.com>
2016-02-02 03:06:41 -0500

    added last scan time to footer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :last_scan_time
+
+  def last_scan_time
+    lastest_scan = Site.select(:last_checked).where(:active => true).order(last_checked: :desc).first
+    lastest_scan.last_checked
+  end
+
+
 end
